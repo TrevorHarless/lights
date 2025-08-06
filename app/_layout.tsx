ddca8916a0f0b287fea7f87ebac08c09ad063f90
tgharles <tgharles@ncsu.edu>
2025-08-06 15:37:14 -0400

    Fixed UI with the status bar to be dark so that it is compatible with the light background of the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { AuthProvider } from "~/contexts/AuthContext";
 
 export default function RootLayout() {
   return (
     <AuthProvider>
+      <StatusBar style="dark" backgroundColor="#ffffff" />
       <Stack screenOptions={{ headerShown: false }} />
     </AuthProvider>
   );
